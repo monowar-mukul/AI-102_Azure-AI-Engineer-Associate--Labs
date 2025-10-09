b38ea13ab61f56696161cf45f10fc0a659efcc62
Monowar Mukul <37457408+asiandevs@users.noreply.github.com>
2025-10-09 12:08:28 +1100

    transcribe your input file
    The program should transcribe your input file and translate it to the language you specified (French, Spanish, or Hindi).

---
+++ 02_translator.py
@@ -0,0 +1,65 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+# Import namespaces
+from azure.core.credentials import AzureKeyCredential
+import azure.cognitiveservices.speech as speech_sdk
+
+def main():
+    try:
+        global speech_config
+        global translation_config
+
+        # Get Configuration Settings
+        load_dotenv()
+        speech_key = os.getenv('KEY')
+        speech_region = os.getenv('REGION')
+
+        # Configure translation
+        # Configure translation
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(speech_key, speech_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from', translation_config.speech_recognition_language)
+
+        # Configure speech
+        # Configure speech
+        speech_config = speech_sdk.SpeechConfig(speech_key, speech_region)
+        print('Ready to use speech service in:', speech_config.region)
+
+        # Get user input
+        targetLanguage = ''
+        while targetLanguage != 'quit':
+            targetLanguage = input(
+                '\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n'
+            ).lower()
+            if targetLanguage in translation_config.target_languages:
+                Translate(targetLanguage)
+            else:
+                targetLanguage = 'quit'
+
+    except Exception as ex:
+        print(ex)
+
+def Translate(targetLanguage):
+    translation = ''
+
+    # Translate speech
+    # Translate speech
+    current_dir = os.getcwd()
+    audioFile = current_dir + '/station.wav'
+    audio_config_in = speech_sdk.AudioConfig(filename=audioFile)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config=audio_config_in)
+    print("Getting speech from file...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
+
+    # Synthesize translation
+
+if __name__ == "__main__":
+    main()
