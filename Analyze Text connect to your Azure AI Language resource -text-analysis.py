def47db71b6d50cec30f0a59bf1211940f3059cc
Monowar Mukul <37457408+monowar-mukul@users.noreply.github.com>
2026-02-11 18:22:51 +1100

    Uncomment text analysis code for reviews

--- Analyze Text connect to your Azure AI Language resource -text-analysis.py
+++ Analyze Text connect to your Azure AI Language resource -text-analysis.py
@@ -25,14 +25,14 @@ def main():
         # -----------------------------
         # Analyze each text file in the reviews folder
         # -----------------------------
-        #reviews_folder = 'reviews'
-        #for file_name in os.listdir(reviews_folder):
-         #   file_path = os.path.join(reviews_folder, file_name)
-          #  with open(file_path, encoding='utf8') as f:
-           #     text = f.read()
+        reviews_folder = 'reviews'
+        for file_name in os.listdir(reviews_folder):
+            file_path = os.path.join(reviews_folder, file_name)
+            with open(file_path, encoding='utf8') as f:
+                text = f.read()
 
-           # print('\n-------------\n' + file_name)
-            #print('\nText:\n' + text)
+            print('\n-------------\n' + file_name)
+            print('\nText:\n' + text)
 
             # -----------------------------
             # Get language
@@ -43,35 +43,35 @@ def main():
             # -----------------------------
             # Get sentiment
             # -----------------------------
-           # sentiment_analysis = ai_client.analyze_sentiment(documents=[text])[0]
-          #  print("Sentiment: {}".format(sentiment_analysis.sentiment))
+            #sentiment_analysis = ai_client.analyze_sentiment(documents=[text])[0]
+            #print("Sentiment: {}".format(sentiment_analysis.sentiment))
 
             # -----------------------------
             # Get key phrases
             # -----------------------------
             #key_phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
             #if key_phrases:
-             #   print("\nKey Phrases:")
-              #  for phrase in key_phrases:
-               #     print('\t{}'.format(phrase))
+            #    print("\nKey Phrases:")
+            #    for phrase in key_phrases:
+            #        print('\t{}'.format(phrase))
 
             # -----------------------------
             # Get entities
             # -----------------------------
-            entities = ai_client.recognize_entities(documents=[text])[0].entities
-            if entities:
-                print("\nEntities:")
-                for entity in entities:
-                    print('\t{} ({})'.format(entity.text, entity.category))
+            #entities = ai_client.recognize_entities(documents=[text])[0].entities
+            #if entities:
+            #    print("\nEntities:")
+            #    for entity in entities:
+            #        print('\t{} ({})'.format(entity.text, entity.category))
 
             # -----------------------------
             # Get linked entities
             # -----------------------------
             #linked_entities = ai_client.recognize_linked_entities(documents=[text])[0].entities
             #if linked_entities:
-              #  print("\nLinked Entities:")
-               # for linked_entity in linked_entities:
-                #    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
+            #    print("\nLinked Entities:")
+            #    for linked_entity in linked_entities:
+            #        print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
     except Exception as ex:
         print("Error:", ex)
